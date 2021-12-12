6790d210094cf0d3334332d81c27593154d38631
nes-kar <enes.kandemir.46@gmail.com>
2021-12-12 20:07:54 +0300

    Seperate Students Based on Their Departments

--- Week 8/departments.py
+++ Week 8/departments.py
@@ -1,12 +1,13 @@
-""" Simple list:
+# Simple list of students:
 li_students=[{"Name":"XXX YYY","Department":"Chemistry"},
 			 {"Name":"AAA BBB","Department":"Physics"}, 
 			 {"Name":"TTT VVV", "Department":"EE"},
 			 {"Name":"LLL MMM","Department":"Chemistry"},
 			 {"Name":"Enes Kandemir","Department":"Physics"},
 			 {"Name":"NNN NNN","Department":"Biology"},
+			 {"Name":"CCC DDD","Department":"Physics"},
 			 {"Name":"MMM MMM","Department":"Biology"}]
-"""
+
 
 departments = [student["Department"] for student in li_students]
 
@@ -14,7 +15,11 @@ departments = list(set(departments)) # Remove the duplicates
 
 for department in departments:
 	f = open(department + ".txt", "w")
+	l = [] # Temporary list to hold the corresponding names.
 	for student in li_students:
 		if student["Department"] == department:
-			f.write("{}\n".format(student["Name"]))
+			l.append(student["Name"])
+	l.sort()
+	for name in l:
+		f.write("{}\n".format(name))
 	f.close()
